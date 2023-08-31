7a47e417d690aad1f43a0140fdbdccd0c03598ee
Anelfry Lora <57969353+anelfry-lora@users.noreply.github.com>
2023-08-31 03:04:29 -0400

    Update index.js

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -4,26 +4,6 @@ import axiosClient from "../axios";
 const store = createStore({
     state: {
         user: {
-<<<<<<< HEAD
-<<<<<<< HEAD
-            data:{name: 'Katana'},
-            token: null
-=======
-            data: {
-                name: 'Anelfry Lora',
-                email: 'correo@email.com',
-                imageUrl: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            },
-            token: 123
->>>>>>> first commit, config proyect whit vue
-        }
-    },
-    getters: {},
-    actions: {},
-<<<<<<< HEAD
-    mutations: {},
-=======
-=======
             data: {},
             token: sessionStorage.getItem('TOKEN')
         },
@@ -220,7 +200,6 @@ const store = createStore({
             }, 3000);
         }
     },
->>>>>>> first commit, config proyect whit vue
     modules: {}
 })
 
